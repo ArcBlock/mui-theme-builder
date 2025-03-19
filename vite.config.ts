19395247295859c4c5fd60de2a8d273e585280c8
july <july@test.com>
2025-03-19 14:47:21 +0800

    chore: support typescript

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { createBlockletPlugin } from 'vite-plugin-blocklet';
 import monacoEditorPlugin from 'vite-plugin-monaco-editor';
 import svgr from 'vite-plugin-svgr';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
