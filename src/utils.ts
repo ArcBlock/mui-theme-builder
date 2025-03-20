c3be10aa6d80d9b5abea29a3fa7e328a0e1e9e27
july <july@test.com>
2025-03-21 07:11:22 +0800

    fix: base assets path

--- src/utils.ts
+++ src/utils.ts
@@ -67,3 +67,17 @@ export function verbose(...args: any[]) {
     console.log(...args);
   }
 }
+
+export function getAuthHeaders() {
+  const headers: Record<string, any> = {};
+  const urlParams = new URLSearchParams(window.location.search);
+  const authKey = urlParams.get('authKey');
+  if (authKey) {
+    const authToken = window.localStorage.getItem(authKey);
+    if (authToken) {
+      headers.Authorization = `Bearer ${authToken}`;
+    }
+  }
+
+  return headers;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig(() => {
         src: path.resolve(__dirname, './src'),
       },
     },
+    base: '/',
     server: {
       port: 3001,
       open: true,
